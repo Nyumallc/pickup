0d7f68f742f112329bf779fde2278567b49f405b
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-08-21 18:19:26 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -23,36 +23,3 @@ const getProfile = async (liffClient) => {
     })
   })
 };
-
-const mustScrollElements = document.querySelectorAll(".must-scroll");
-for(i=0;i<mustScrollElements.length;i++){
-  const textarea = mustScrollElements[i].querySelector("textarea");
-  const checkbox = mustScrollElements[i].querySelector("input[type='checkbox']");
-  const button = mustScrollElements[i].querySelector("input[type='button']");
-  const label = mustScrollElements[i].querySelector("label");
-
-  let buttonFor
-  if(button&&button.hasAttribute("for")){
-    buttonFor = button.attributes["for"].value;
-  }
-
-  let scrolled = false;
-  if(textarea){
-    textarea.addEventListener("scroll", func=()=>{
-      if(!scrolled && Math.abs(textarea.scrollHeight-textarea.clientHeight-textarea.scrollTop)<1){
-        scrolled = true;
-        if(checkbox){checkbox.disabled=false;}
-        if(label){label.classList.remove("disabled");}
-        if(!buttonFor){
-          if(button){button.disabled=false;}
-        }
-      }
-    });
-  }
-  if(buttonFor){
-    const mustCheck = document.querySelector("input#" + buttonFor);
-    mustCheck.addEventListener("click", func=()=>{
-      button.disabled = !mustCheck.checked;
-    });
-  }
-};
