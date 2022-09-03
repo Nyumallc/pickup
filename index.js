7510244c0b1636eae90c5ba1da910263a4ec306f
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-09-04 01:36:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function pushButton1() {
 
 
     
-    let resurl=(`${REQUEST_URL}?&userid=${userid}&displayname=${displayname}&order_number=${order_numbertext}&shipping_numbertext=${shipping_numbertext}&item_count=${item_counttext}
+    let resurl=(`${REQUEST_URL}?&userid=${userid}&displayname=${displayname}&order_number=${order_numbertext}&shipping_number=${shipping_numbertext}&item_count=${item_counttext}
     &package=${packagetext}&shipping_select=${shipping_selecttext}&option=${optiontext}`)
     let res_text=(`${displayname}${order_numbertext}已收到您的訂單`)
          alert(res_text);
